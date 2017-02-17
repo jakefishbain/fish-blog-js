09372aaed294ecd16d9b73d8f28b434530bbace7
Jake Fishbain <jakefishbain@fish.local>
2017-02-17 10:14:17 -0600

    setup and header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BlogHeader from './BlogHeader';
 
 class App extends Component {
+  //CONSTRUCTOR FOR STATE
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BlogHeader/>
       </div>
     );
   }
---
+++ src/BlogHeader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class BlogHeader extends Component {
+	render(){
+		return(
+			<header>Welcome to my Blog!</header>
+		)
+	}
+}
+export default BlogHeader;
