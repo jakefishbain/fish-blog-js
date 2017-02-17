7c3e0d701d99a106a489f62f3c6a282852442384
Jake Fishbain <jakefishbain@fish.local>
2017-02-17 13:22:33 -0600

    create action btn rendering fn, move edit and save

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class BlogPost extends Component {
+	renderActionBtn(){
+		if(this.props.isEditing){
+			return <button className='saveBtn' onClick={() => this.props.onToggleEdit(this.props.id)}>Save</button>
+		}else{
+			return <button className='editBtn' onClick={() => this.props.onToggleEdit(this.props.id)}>Edit</button>
+		}
+	}
+
 	render(){
 		return(
 			<li className='blogPost'>
 				<p className='postContent'>{this.props.content}</p>
 				<p className='postAuthor'>Post by: {this.props.author} on {this.props.date}</p>
-						{/*ADD EDIT FUNCTIONALITY HERE*/}
-				<button className='editBtn'>Edit</button>
+				{this.renderActionBtn()}
 				<button onClick={() => this.props.onDelete(this.props.id)} className='deleteBtn'>🗑</button>
 			</li>
 		)
