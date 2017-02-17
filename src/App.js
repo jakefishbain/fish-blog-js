9188c9f042c1d6a422e7be23efcc4b2e8162ef12
Jake Fishbain <jakefishbain@fish.local>
2017-02-17 10:53:36 -0600

    add base state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import BlogHeader from './BlogHeader';
-import BlogInputs from './BlogInputs'
+import BlogInputs from './BlogInputs';
+import BlogPost from './BlogPost'
+var moment = require('moment');
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      blogPosts: [
+        {content: 'content goes here', author: 'Jake', date: {moment().toString()}},
+        {content: 'content goes here2', author: 'Jake F', date: {moment().toString()}},
+        {content: 'content goes here3', author: 'Jake Fi', date: {moment().toString()}},
+        {content: 'content goes here4', author: 'Jake Fis', date: {moment().toString()}},
+        {content: 'content goes here5', author: 'Jake Fish', date: {moment().toString()}},
+      ]
+    }
+  }
   //CONSTRUCTOR FOR STATE
 
   render() {
@@ -11,6 +25,11 @@ class App extends Component {
       <div className="App">
         <BlogHeader/>
         <BlogInputs/>
+        <ul>
+          {
+            <BlogPost/>
+          }
+        </ul>
       </div>
     );
   }
